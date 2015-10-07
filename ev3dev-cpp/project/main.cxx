47e88d35a9258566896b24e84057c9bb0d306da9
Gamer_Z <grasmanek94@gmail.com>
2015-10-07 23:44:47 +0200

    GripControl class

--- ev3dev-cpp/project/main.cxx
+++ ev3dev-cpp/project/main.cxx
@@ -14,15 +14,136 @@
 
 #include <lib/ev3dev/ev3dev.hxx>
 
+/*
+	Run this class in another thread because this blocks the thread (all motor control functions do)
+*/
+class GripControl
+{
+public:
+	enum State
+	{
+		StateOpen,
+		StateClosed,
+		StateOpening,
+		StateClosing,
+		StateUnknown
+	};
+
+private:
+	static const int		_grip_amount = 2050;//90 degree turn
+	ev3dev::medium_motor	_grip;
+	ev3dev::touch_sensor	_touch_sensor;
+	State					state;
+
+	bool _sensor_is_open() const
+	{
+		return _touch_sensor.value();
+	}
+
+public:
+	GripControl()
+		:	_grip(ev3dev::OUTPUT_B),
+		_touch_sensor(ev3dev::INPUT_1)
+	{
+		Reset();
+		
+		if (Available())
+		{
+			//Only reliable way to determine state is by the touch sensor, so use it!
+			if (_sensor_is_open())
+			{
+				state = StateOpen;
+			}
+			else
+			{
+				state = StateClosed;
+				Open();
+			}
+		}
+		else
+		{
+			state = StateUnknown;
+		}
+	}
+
+	void Reset()
+	{
+		if (_grip.connected())
+		{
+			_grip.reset();
+		}
+	}
+
+	State GetState() const
+	{
+		return state;
+	}
+
+	bool Close()
+	{
+		if (_sensor_is_open() && 
+			state == StateOpen)
+		{
+
+			state = StateClosing;
+
+			_grip
+				.set_position_sp(-_grip_amount)
+				.set_duty_cycle_sp(100)
+				.run_to_rel_pos();
+
+			while (_grip.state().count("running"))
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+
+			state = StateClosed;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	bool Open()
+	{
+		if (!_sensor_is_open() &&
+			state == StateClosed)
+		{
+
+			state = StateOpening;
+
+			_grip
+				.set_duty_cycle_sp(100)
+				.run_forever();
+
+			while (!_sensor_is_open())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+
+			_grip.stop();
+
+			state = StateOpen;
+		}
+
+		return false;
+	}
+
+	bool Available() const
+	{
+		return _grip.connected() && _touch_sensor.connected();
+	}
+};
+
 class MotorControl
 {
 public:
 	MotorControl()
-		: _left(ev3dev::OUTPUT_A), 
-		_right(ev3dev::OUTPUT_D), 
-		_grip(ev3dev::OUTPUT_B)
+		:	_left(ev3dev::OUTPUT_A), 
+			_right(ev3dev::OUTPUT_D)
 	{
-		//Reset();
+		Reset();
 	}
 
 	~MotorControl()
@@ -41,11 +162,6 @@ public:
 		{
 			_right.reset();
 		}
-
-		if(_grip.connected())
-		{
-			_grip.reset();
-		}
 	}
 
 	void Move(int speed)
@@ -56,46 +172,29 @@ public:
 		_right.run_forever();
 	}
 
-	void Close()
-	{
-		_grip
-			.set_position_sp(10000)
-			.set_duty_cycle_sp(33)
-			.run_to_rel_pos();
-	}
-
-	void Open()
-	{
-		_grip
-			.set_position_sp(-10000)
-			.set_duty_cycle_sp(33)
-			.run_to_rel_pos();
-	}
-
 	bool Available() const
 	{
-		return _left.connected() && _right.connected() && _grip.connected();
+		return _left.connected() && _right.connected();
 	}
 
 private:
 	ev3dev::large_motor     _left;
 	ev3dev::large_motor     _right;
-	ev3dev::medium_motor    _grip;
+
 
 };
 
 int main()
 {
-	MotorControl control;
+	GripControl control;
 
 	try
 	{
 		if (control.Available())
 		{
 			std::cout << "Running motors" << std::endl;
-			control.Close();
-			std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 			control.Open();
+			control.Close();
 		}
 		else
 		{
