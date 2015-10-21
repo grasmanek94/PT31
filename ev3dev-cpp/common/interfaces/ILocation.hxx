7bdc8d3fd5ff93ef1f917dd48630b5c0743a6362
Gamer_Z <grasmanek94@gmail.com>
2015-10-21 13:58:31 +0200

    fix anomalities

--- ev3dev-cpp/common/interfaces/ILocation.hxx
+++ ev3dev-cpp/common/interfaces/ILocation.hxx
@@ -8,6 +8,7 @@ class ILocation
 public:
 	//return x y z angle
 	virtual glm::vec4 GetLocation() = 0;
+	virtual bool Available() = 0;
 };
 
 #endif
--- ev3dev-cpp/common/interfaces/IMecanumControl.hxx
+++ ev3dev-cpp/common/interfaces/IMecanumControl.hxx
@@ -7,7 +7,7 @@ class IMecanumControl : public IDriveControl
 {
 public:
 	virtual void Strafe(int speed, Direction direction, float centimeters) = 0;//move sideways with e.g. Mecanum Wheels
-	virtual void MoveTurn(int moveSpeed, Direction moveDirection, float moveCentimeters, int turnSpeed, float turnDegrees) = 0;
+	virtual void MoveTurn(int moveSpeed, Direction moveDirection, float moveCentimeters, int turnSpeed, Direction turnDirection, float turnDegrees) = 0;
 };
 
 #endif
