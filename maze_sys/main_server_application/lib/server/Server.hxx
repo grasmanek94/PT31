a12561446ec65815046e8b2244b730bc87d3ba32
Gamer_Z <grasmanek94@gmail.com>
2015-12-09 11:18:49 +0100

    serverrr

--- maze_sys/main_server_application/lib/server/Server.hxx
+++ maze_sys/main_server_application/lib/server/Server.hxx
@@ -6,6 +6,8 @@
 #include <PathProcessor/PathProcessorQueue.hxx>
 #include "ServBot.hxx"
 
+#include <enet/enetpp.hxx>
+
 template <size_t max_bots = 2, size_t item_max_bytes = 2048, size_t per_queue_max_items = 128>
 class Server
 {
@@ -21,10 +23,42 @@ private:
 	PPQ::MyIPCQueue* pCalculated;
 	PPQ::MyIPCQueue* pRequested;
 
+	NetworkServer* ns;
+
+	void RunNetworking()
+	{
+		if (ns->Pull())
+		{
+			ENetEvent event = ns->Event();
+			switch (event.type)
+			{
+			case ENET_EVENT_TYPE_CONNECT:
+				printf("A new client connected from %x:%u.\n",
+					event.peer->address.host,
+					event.peer->address.port);
+				/* Store any relevant client information here. */
+				event.peer->data = "Client information";
+				break;
+			case ENET_EVENT_TYPE_RECEIVE:
+				printf("%s\n", event.packet->data);
+				/* Clean up the packet now that we're done using it. */
+				enet_packet_destroy(event.packet);
+
+				break;
+
+			case ENET_EVENT_TYPE_DISCONNECT:
+				printf("%s disconnected.\n", event.peer->data);
+				/* Reset the peer's client information. */
+				event.peer->data = NULL;
+				break;
+			}
+		}
+	}
 public:
 	Server()
 		:	positions(new PosArray()),
-			pathprocessorqueues(new PPQ())
+			pathprocessorqueues(new PPQ()),
+			ns(new NetworkServer())
 	{
 		for (size_t i = 0; i < max_bots; ++i)
 		{
@@ -33,7 +67,16 @@ public:
 
 		pCalculated = pathprocessorqueues->Calculated();
 		pRequested = pathprocessorqueues->Request();
+
+		if (ns->GetInitCode()
+			|| !ns->Create(8)
+			|| !ns->Good())
+		{
+			std::cout << "An error occurred while trying to create an ENet object." << std::endl;
+			exit(EXIT_FAILURE);
+		}
 	}
+
 
 	void Run()
 	{
@@ -46,12 +89,15 @@ public:
 				robots[item.GetOperationIdentifier()].ToDo()->Push(&item);
 			}
 		}
+
+		RunNetworking();
 	}
 
 	~Server()
 	{
 		delete positions;
 		delete pathprocessorqueues;
+		delete ns;
 	}
 };
 #endif
